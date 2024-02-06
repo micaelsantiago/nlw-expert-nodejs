edae07628d66d6b1829482c68c6d74c738281c68
Micael Santiago <83787168+micaelsantiago@users.noreply.github.com>
2024-02-06 19:46:16 -0300

    feat: add /polls route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,25 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod';
 
 const app = fastify();
+const prisma = new PrismaClient();
+
+app.post('/polls', async (response, reply) => {
+  const createPollBody = z.object({
+    title: z.string()
+  });
+
+  const { title } = createPollBody.parse(response.body);
+
+  const poll = await prisma.poll.create({
+    data: {
+      title
+    }
+  });
+
+  return reply.status(201).send({ pollId: poll.id });
+});
 
 app.listen({ port: 3000 }).then(() => {
   console.log('HTTP server running!');
