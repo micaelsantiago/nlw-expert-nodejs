99a193ef3e5cf635b914cea7f28f70b2819abaf2
Micael Santiago <83787168+micaelsantiago@users.noreply.github.com>
2024-02-07 18:57:52 -0300

    build: add cookie

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,16 @@
 import fastify from 'fastify';
+import cookie from '@fastify/cookie';
 import { createPoll } from '../routes/create-poll';
 import { getPoll } from '../routes/get-poll';
+import { voteOnPoll } from '../routes/vote-on-poll';
 
 const app = fastify();
 
+app.register(cookie);
+
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3000 }).then(() => {
   console.log('HTTP server running!');
