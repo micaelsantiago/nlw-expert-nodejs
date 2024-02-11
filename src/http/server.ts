3aa0bc519f4afa85c1459c710a839f732b18acda
Micael Santiago <83787168+micaelsantiago@users.noreply.github.com>
2024-02-11 18:23:11 -0300

    project completion

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,9 +3,12 @@ import cookie from '@fastify/cookie';
 import { createPoll } from '../routes/create-poll';
 import { getPoll } from '../routes/get-poll';
 import { voteOnPoll } from '../routes/vote-on-poll';
+import fastifyWebsocket from '@fastify/websocket';
+import { pollResults } from './ws/poll-results';
 
 const app = fastify();
 
+app.register(fastifyWebsocket);
 app.register(cookie, {
   secret: 'polls-app-nlw',
   hook: 'onRequest'
@@ -14,6 +17,7 @@ app.register(cookie, {
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+app.register(pollResults);
 
 app.listen({ port: 3000 }).then(() => {
   console.log('HTTP server running!');
--- src/routes/vote-on-poll.ts
+++ src/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import z from "zod";
 import { prisma } from "../lib/prisma";
 import { FastifyInstance } from "fastify";
 import { randomUUID } from "node:crypto";
+import { redis } from "../lib/redis";
+import { voting } from "../utils/voting-pub-sub";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/vote', async (request, reply) => {
@@ -34,6 +36,13 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id
           }
         });
+
+        const votes = await redis.zincrby(pollId, -1, pollOptionId);
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.pollOptionId
+          votes: Number(votes)
+        })
       }
       else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({
@@ -60,6 +69,13 @@ export async function voteOnPoll(app: FastifyInstance) {
         sessionId
       }
     });
+
+    const votes = await redis.zincrby(pollId, 1, pollOptionId);
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes)
+    })
 
     return reply.status(201).send();
   });
